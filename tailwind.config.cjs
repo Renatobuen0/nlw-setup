7b49998ef9f85848c42590829c05763a6ab2bed3
Renato Bueno 🧑🏻‍💻 <renato.bueno@performait.com>
2023-01-22 13:16:28 -0300

    Format filers

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,19 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/**/*.tsx',
-    './index.html'
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: '#09090A'
-      },
+	content: ["./src/**/*.tsx", "./index.html"],
+	theme: {
+		extend: {
+			colors: {
+				background: "#09090A",
+			},
 
-      gridTemplateRows: {
-        7: 'repeat(7, minmax(0, 1fr))',
-      }
-    },
-  },
-  plugins: [],
+			gridTemplateRows: {
+				7: "repeat(7, minmax(0, 1fr))",
+			},
+		},
+	},
+	plugins: [],
 }
